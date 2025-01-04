59c51bad1fd7714878467531f0c418278eecb28f
anonyxbiz <anonyxbiz@gmail.com>
2025-01-04 17:00:36 +0300

    set-cookie update

--- Blazeio/Modules/request.py
+++ Blazeio/Modules/request.py
@@ -165,7 +165,8 @@ class Request:
                 elif len(__buff__) >= max_buff_size:
                     break
 
-        r.__stream__.appendleft(b'' + __buff__[idx + 4:])
+        if r.__stream__ is not None:
+            r.__stream__.appendleft(b'' + __buff__[idx + 4:])
 
         return r
 
