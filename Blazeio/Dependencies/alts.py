070bb9d8351bb5b945c24d642cafa054daa2525f
anonyxbiz <anonyxbiz@gmail.com>
2025-04-18 03:25:32 +0300

    Automated push (version: 2.0.6.7) on Friday 18'th April 2025 at 00:25:31 UTC

--- Blazeio/Dependencies/alts.py
+++ Blazeio/Dependencies/alts.py
@@ -22,27 +22,24 @@ class __Coro__:
 Coro = __Coro__()
 
 class DictView:
-    __slots__ = ("_dict", "_lower")
+    __slots__ = ("_dict",)
 
     def __init__(app, _dict: dict):
         app._dict = _dict
-        app._lower = None
-
-    def _ensure_cache(app):
-        if app._lower is None or len(app._lower) != len(app._dict):
-            app._lower = {k.lower(): k for k in app._dict}
 
     def __contains__(app, key):
-        app._ensure_cache()
-        return key.lower() in app._lower
+        key = key.lower()
+        for i in app._dict:
+            if i.lower() == key:
+                return True
+        return False
 
     def pop(app, key, default=None):
-        app._ensure_cache()
-        lower_key = key.lower()
-        original_key = app._lower.get(lower_key)
-        if original_key is not None:
-            del app._lower[lower_key]
-            return app._dict.pop(original_key)
+        key = key.lower()
+        for i in app._dict:
+            if i.lower() == key:
+                return app._dict.pop(i, default)
+
         return default
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
     requirements = []
     long_description = ""
 
-version = "2.0.6.5"
+version = "2.0.6.6"
 
 setup(
     name="Blazeio",
