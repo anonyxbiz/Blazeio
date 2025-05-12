8b1f2f81a3ec15ca986f651321331d3cf2ff569e
anonyxbiz <anonyxbiz@gmail.com>
2025-05-12 04:46:01 +0300

    Automated push (version: 2.1.4.0) on Monday 12'th May 2025 at 01:46:00 UTC

--- Blazeio/Extensions/setup_build.py
+++ Blazeio/Extensions/setup_build.py
@@ -1,4 +1,4 @@
-version = "1.1.8.0"
+version = "1.1.9.2"
 
 from argparse import ArgumentParser
 parser = ArgumentParser(prog="Setup", description = "Setup")
--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ with open("%s/requirements.txt" % data_path) as f:
 with open("%s/README.md" % data_path, encoding="utf-8") as f:
     long_description = f.read()
 
-version = "2.1.3.8"
+version = "2.1.3.9"
 
 exts = ("Blazeio_iourllib", "client_payload_gen", "c_request_util")
 
-for ext in exts: system("python %s build --mname %s --mpath %s && python %s install --mname %s --mpath %s" % (path.join(data_path, "Blazeio", "Extensions", "setup_build.py"), ext, path.join(data_path, "Blazeio", "Extensions", ext + ".c"), path.join(data_path, "Blazeio", "Extensions", "setup_build.py"), ext, path.join(data_path, "Blazeio", "Extensions", ext + ".c")))
+for ext in exts: system("%spython %s build --mname %s --mpath %s && python %s install --mname %s --mpath %s" % ("cd %s && " % path.join(data_path, "Blazeio", "Extensions") if not environ.get("BlazeioDev", None) else "", path.join(data_path, "Blazeio", "Extensions", "setup_build.py"), ext, path.join(data_path, "Blazeio", "Extensions", ext + ".c"), path.join(data_path, "Blazeio", "Extensions", "setup_build.py"), ext, path.join(data_path, "Blazeio", "Extensions", ext + ".c")))
 
 setup(
     name="Blazeio",
