62024c19c1d617a17b6386a524860241003a9a2f
anonyxbiz <anonyxbiz@gmail.com>
2025-05-19 21:02:14 +0300

    Automated push (version: 2.1.5.9) on Monday 19'th May 2025 at 18:02:14 UTC

--- setup.py
+++ setup.py
@@ -11,17 +11,24 @@ with open("%s/requirements.txt" % data_path) as f:
 with open("%s/README.md" % data_path, encoding="utf-8") as f:
     long_description = f.read()
 
-version = "2.1.5.7"
+version = "2.1.5.8"
 
 exts = ("Blazeio_iourllib", "client_payload_gen", "c_request_util")
+
+ext_modules = []
 
 for ext in exts:
     if platform.system() == 'Windows':
         system("%spython %s build --mname %s --mpath %s && python %s install --mname %s --mpath %s" % ("cd %s && " % path.join(data_path, "Blazeio", "Extensions"), "setup_build.py", ext, ext + ".c", "setup_build.py", ext, ext + ".c"))
     else:
-        system("python %s build --mname %s --mpath %s && python %s install --mname %s --mpath %s" % (path.join(data_path, "Blazeio", "Extensions", "setup_build.py"), ext, path.join(data_path, "Blazeio", "Extensions", ext + ".c"), path.join(data_path, "Blazeio", "Extensions", "setup_build.py"), ext, path.join(data_path, "Blazeio", "Extensions", ext + ".c")))
+        for module in exts:
+            ext_modules.append(Extension(
+                module,
+                sources=[path.join(data_path, "Blazeio", "Extensions", module + ".c")],
+                extra_compile_args=['-O3'],
+            ))
 
-setup(
+kwargs = dict(
     name="Blazeio",
     version=version,
     description="Blazeio",
@@ -47,3 +54,14 @@ setup(
         ],
     }
 )
+
+if ext_modules:
+    kwargs["ext_modules"] = ext_modules
+    kwargs["options"] = {
+        'build_ext': {
+            'inplace': False if environ.get("BlazeioDev", None) else True,
+            'force': True
+        }
+    }
+
+setup(**kwargs)
