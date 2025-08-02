f41bbd548709d2eb6f47bf4471f1671909e8aa30
anonyxbiz <anonyxbiz@gmail.com>
2025-08-03 01:38:22 +0300

    Automated push (version: 2.3.4.6) on Saturday 02'th August 2025 at 22:38:21 UTC

--- Blazeio/Modules/onrender.py
+++ Blazeio/Modules/onrender.py
@@ -1,40 +1,46 @@
 import Blazeio as io
 
 class RenderFreeTierPatch:
-    _is_on_render = io.is_on_render()
-    def __init__(app, production = NotImplemented, host = None, rnd_host = None, asleep = 5):
+    def __init__(app, host = None, rnd_host = None, asleep = 5):
         for method, value in locals().items():
             if method == app: continue
             setattr(app, method, value)
 
         app.host_resolved = io.SharpEvent()
+        app.request_count = 0
         app.task = io.ioConf.loop.create_task(app.keep_alive_render())
 
     async def _hello_world(app, r):
-        await io.Deliver.text("Hello World")
+        app.request_count += 1
+        await io.Deliver.text("Hello World@%s" % io.token_urlsafe(11))
+
+    async def _rftp_request_count(app, r):
+        await io.Deliver.text("request_count: %s" % app.request_count)
 
     async def keep_alive_render(app):
         await io.plog.debug("keep_alive_render initiating...")
         await app.host_resolved.wait_clear()
 
         await io.plog.debug("keep_alive_render initiated...")
-
-        while True:
-            async with io.Session("%s/hello/world" % app.rnd_host, "get", io.Rvtools.headers) as r:
-                await r.text()
-            await io.sleep(app.asleep)
+        
+        async with io.Ehandler(ignore=io.CancelledError, exit_on_err=True):
+            while True:
+                async with io.getSession("%s/hello/world" % app.host, "get", io.Rvtools.headers) as r:
+                    await r.text()
+                await io.sleep(app.asleep)
 
     async def before_middleware(app, r):
-        if not app.rnd_host and not app.host:
-            if not (host := r.headers.get("Referer", r.headers.get("Origin"))):
+        if not app.host:
+            if not (url := r.headers.get("Referer", r.headers.get("Origin", r.headers.get("Host")))):
                 return
 
-            if (id1 := host.find(v := "://")) != -1:
-                if (id2 := host[(id1 := id1 + len(v)):].find("/")) != -1:
-                    host = host[:id1 + id2]
+            try: app.host, port, _ = io.ioConf.url_to_host(url, {})
+            except TypeError: return
 
-            app.rnd_host = host
-            app.host = host
-
-            await io.plog.info("Added host as: %s" % app.rnd_host)
+            if port == 443:
+                app.host = "https://%s" % app.host
+            else:
+                app.host = "http://%s:%s" % (app.host, port)
+    
+            await io.plog.info("Added host as: %s" % app.host)
             app.host_resolved.set()
