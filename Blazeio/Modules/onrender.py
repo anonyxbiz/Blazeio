cf4f20f6df0f8680128a81dacb656b17cf90e39e
anonyxbiz <anonyxbiz@gmail.com>
2025-08-08 17:01:29 +0300

    Automated push (version: 2.3.5.2) on Friday 08'th August 2025 at 14:01:27 UTC

--- Blazeio/Modules/onrender.py
+++ Blazeio/Modules/onrender.py
@@ -1,13 +1,14 @@
 import Blazeio as io
 
 class RenderFreeTierPatch:
-    def __init__(app, host = None, rnd_host = None, asleep = 60):
+    def __init__(app, host = None, rnd_host = None, asleep = 30):
         for method, value in locals().items():
             if method == app: continue
             setattr(app, method, value)
 
         app.host_resolved = io.SharpEvent()
         app.request_count = 0
+        app.e = None
         app.task = io.ioConf.loop.create_task(app.keep_alive_render())
 
     async def _hello_world(app, r):
@@ -15,18 +16,21 @@ class RenderFreeTierPatch:
         await io.Deliver.text("Hello World@%s" % io.perf_counter())
 
     async def _rftp_request_count(app, r):
-        await io.Deliver.text("request_count: %s" % app.request_count)
+        await io.Deliver.json(io.ddict(request_count = app.request_count, e = app.e))
 
     async def keep_alive_render(app):
         await io.plog.debug("keep_alive_render initiating...")
         await app.host_resolved.wait_clear()
-
         await io.plog.debug("keep_alive_render initiated...")
-        
-        while True:
-            async with io.Session("%s/hello/world" % app.host, "get", io.Rvtools.headers) as r:
-                await r.text()
-            await io.sleep(app.asleep)
+
+        async with io.Ehandler() as e:
+            while True:
+                async with io.Session("%s/hello/world" % app.host, "get", io.Rvtools.headers, params = io.ddict(perf_counter=io.perf_counter())) as r:
+                    await r.text()
+                await io.sleep(app.asleep)
+
+        if e.err:
+            app.e = str(e.err)
 
     async def before_middleware(app, r):
         if not app.host:
