1913b6286df7baea1cb8f019bd526f3cc079452b
anonyxbiz <anonyxbiz@gmail.com>
2025-08-16 17:05:55 +0300

    Automated push (version: 2.3.7.1) on Saturday 16'th August 2025 at 14:05:54 UTC

--- Blazeio/Protocols/multiplexer.py
+++ Blazeio/Protocols/multiplexer.py
@@ -486,7 +486,7 @@ class BlazeioClientProtocol(BlazeioMuxProtocol(io.BlazeioClientProtocol)):
     __stream_closed_exception__ = io.ServerDisconnected
 
     def __getattr__(app, name):
-        return getattr(app.multiplexer, name)
+        return getattr(app.multiplexer, name, None)
 
     def create_stream(app, _id: (None, bytes) = None):
         return app.multiplexer.enter_stream(_id, Blazeio_Stream_Client)
