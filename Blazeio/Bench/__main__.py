3a7703949ba41d02a04b55f1b8fab23a2e876b06
anonyxbiz <anonyxbiz@gmail.com>
2025-10-10 02:20:35 +0300

    Automated push (version: 2.6.5.5) on Thursday 09'th October 2025 at 23:20:34 UTC

--- Blazeio/Bench/__main__.py
+++ Blazeio/Bench/__main__.py
@@ -5,6 +5,19 @@ io.ioConf.OUTBOUND_CHUNK_SIZE, io.ioConf.INBOUND_CHUNK_SIZE = 1024*4, 1024*4
 
 web = io.App("0.0.0.0", 6000, name = "web")
 
+class Utils:
+    def __init__(app): ...
+
+    def remaining_time(app):
+        if not app.perf_counter: return 1
+        return (app.d - (io.perf_counter() - app.perf_counter))
+
+    async def log_timing(app, lineno: int = 10):
+        async with app.sync_serializer:
+            while int(remaining_time := app.remaining_time()):
+                await io.plog.yellow("<line_%d>" % lineno, "detail: Bench Running", "conns: %d" % len(app.conns), "remaining_time: %s" % remaining_time, func = app.log_timing)
+                await io.sleep(0.05)
+
 class Server:
     def __init__(app):
         app.payload = b"." * app.payload_size
@@ -15,13 +28,14 @@ class Server:
             b'Server: %b\r\n' % ("Blazeio/%s (%s)" % (io.__version__, io.os_name)).encode() +
             b'Content-Type: text/plain\r\n' +
             b'Content-disposition: inline; filename="%b.txt"\r\n' % r.path.encode() +
-            b'Content-Length: %d\r\n' % len(content) +
+            b'Content-Length: %d\r\n' % (len(content)*app.writes) +
             b'\r\n'
         )
         
         if r.method in r.no_response_body_methods: raise io.Eof()
 
-        await r.writer(content)
+        for i in range(app.writes):
+            await r.writer(content)
 
 class Client:
     def __init__(app): ...
@@ -31,51 +45,44 @@ class Client:
 
         analytics = io.ddict(requests = [])
 
-        async with io.Session(app.url, prepare_http = False, send_headers = False, connect_only = True) as conn:
-            while int(app.available_time()):
-                analytics.requests.append(request := io.ddict(latency = io.perf_timing(), ttfb = io.perf_timing(), prepare = io.perf_timing(), request_info = io.ddict()))
+        conn = io.Session(app.url, prepare_http = False, send_headers = False, connect_only = True)
 
-                r = await conn.prepare((app.url % "/tests/io/raw/get") if app.url.startswith("http://%s:%d" % (web.ServerConfig.host, web.ServerConfig.port)) else app.url, app.m.upper(), prepare_http = False) # Convert the objects into a http/1.1 request and send it without preparing the response — No I/O involved here.
+        while int(app.remaining_time()):
+            analytics.requests.append(request := io.ddict(latency = io.perf_timing(), ttfb = io.perf_timing(), prepare = io.perf_timing(), request_info = io.ddict(), duration = io.perf_timing()))
 
-                request.prepare = request.prepare.get()
+            r = await conn.prepare((app.url % "/tests/io/raw/get") if app.url.startswith("http://%s:%d" % (web.ServerConfig.host, web.ServerConfig.port)) else app.url, app.m.upper(), prepare_http = False) # Convert the objects into a http/1.1 request and send it without preparing the response — No I/O involved here.
 
-                request.ttfb_io = io.perf_timing() # Calculate the time it takes for a chunk to be received from server — waiting I/O involved here.
+            request.prepare = request.prepare.get()
 
-                await r.protocol.ensure_reading() # Ensure the transport is reading and wait for a chunk to be received from server if no chunk has been received yet.
+            request.ttfb_io = io.perf_timing() # Calculate the time it takes for a chunk to be received from server — waiting I/O involved here.
 
-                request.ttfb_io = request.ttfb_io.get()
+            await r.protocol.ensure_reading() # Ensure the transport is reading and wait for a chunk to be received from server if no chunk has been received yet.
 
-                request.ttfb = request.ttfb.get()
+            request.ttfb_io = request.ttfb_io.get()
 
-                request.prepare_http = io.perf_timing()
+            request.ttfb = request.ttfb.get()
 
-                await r.prepare_http() # Receive chunks upto the http crlf that marks end of headers and build the request objects
+            request.prepare_http = io.perf_timing()
 
-                request.prepare_http = request.prepare_http.get()
+            await r.prepare_http() # Receive chunks upto the http crlf that marks end of headers and build the request objects
 
-                request.request_info.path, request.request_info.server_software = r.path, r.headers.get("server")
+            request.prepare_http = request.prepare_http.get()
 
-                request.body_io = io.perf_timing()
+            request.request_info.path, request.request_info.server_software = r.path, r.headers.get("server")
 
-                body = await r.aread() # Receive the response body
+            request.body_io = io.perf_timing()
 
-                request.body_io = request.body_io.get()
+            body = bytearray()
+            async for chunk in r:
+                body.extend(chunk)
 
-                request.latency = request.latency.get()
+            request.body_io = request.body_io.get()
+            request.transferred_bytes = len(body)
+
+            request.latency = request.latency.get()
+            request.duration = request.duration.get().elapsed
 
         return analytics
-
-class Utils:
-    def __init__(app): ...
-
-    def available_time(app):
-        return (app.d - (io.perf_counter() - app.perf_counter))
-
-    async def log_timing(app, lineno: int = 10):
-        async with app.sync_serializer:
-            while int(app.available_time()):
-                await io.plog.yellow("<line_%d>" % lineno, "detail: Bench Running", "conns: %d" % len(app.conns), "remaining_time: %s" % app.available_time(), func = app.log_timing)
-                await io.sleep(0.05)
 
 class Runner(Client, Utils):
     def __init__(app):
@@ -89,10 +96,11 @@ class Runner(Client, Utils):
         for i in range(app.c):
             app.conns.append(io.getLoop.create_task(app.client(i)))
 
-        app.perf_counter = io.perf_counter()
         io.getLoop.create_task(app.log_timing())
 
         if app.serialize_connections: await app.serializer.__aexit__()
+
+        app.perf_counter = io.perf_counter()
 
         analytics = await io.gather(*app.conns)
 
@@ -109,7 +117,10 @@ class Runner(Client, Utils):
                         for metric_type in app.metric_types
                     }
                     for metric in app.request_metrics
-                }
+                },
+                duration = (duration := sum([sum([request.duration for request in i.requests]) for i in analytics])/len(analytics)),
+                transferred_mbs = (transferred_mbs := sum([sum([request.transferred_bytes for request in i.requests]) for i in analytics])/(1024**2)),
+                transfer_rate = (transferred_mbs/duration)
             ),
         )), func = app.runner)
 
@@ -117,13 +128,13 @@ class Runner(Client, Utils):
         raise KeyboardInterrupt()
 
 class Main(Server, Runner):
-    __slots__ = ("c", "d", "payload_size", "url", "m", "payload", "serialize_connections", "conns")
+    __slots__ = ("c", "d", "payload_size", "url", "m", "payload", "serialize_connections", "conns", "writes")
     serializer = io.ioCondition()
     sync_serializer = io.ioCondition()
     request_metrics = ("prepare", "prepare_http", "ttfb_io", "ttfb", "body_io", "latency")
     metric_types = ("elapsed", "rps")
 
-    def __init__(app, url: (str, io.Utype) = ("http://%s:%d" % (web.ServerConfig.host, web.ServerConfig.port)) + "%s", c: (int, io.Utype) = 100, d: (int, io.Utype) = 10, m: (str, io.Utype) = "head", payload_size: (int, io.Utype) = 1024, conns: (list, io.Unone) = [], serialize_connections: (bool, io.Utype) = True, perf_counter: (None, io.Unone) = None):
+    def __init__(app, url: (str, io.Utype) = ("http://%s:%d" % (web.ServerConfig.host, web.ServerConfig.port)) + "%s", c: (int, io.Utype) = 100, d: (int, io.Utype) = 10, m: (str, io.Utype) = "get", payload_size: (int, io.Utype) = 1024, writes: (int, io.Utype) = 100, conns: (list, io.Unone) = [], serialize_connections: (bool, io.Utype) = True, perf_counter: (None, io.Unone) = None):
         io.set_from_args(app, locals(), (io.Utype, io.Unone))
         io.Super(app).__init__()
 
