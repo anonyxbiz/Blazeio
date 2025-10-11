508f43afd7627081dab4aa6c6acd5aba95b4365d
anonyxbiz <anonyxbiz@gmail.com>
2025-10-12 00:06:43 +0300

    Automated push (version: 2.7.1.4) on Saturday 11'th October 2025 at 21:06:42 UTC

--- Blazeio/Other/proxy.py
+++ Blazeio/Other/proxy.py
@@ -65,7 +65,7 @@ class Dbstuff:
                     app.updaters_coordination.previous_size = size
                     async with app.updaters_coordination.sync:
                         app.hosts.update(io.Dotify(io.loads(await io.aread(app.blazeio_proxy_hosts))))
-                        await io.plog.cyan("loaded hosts from: %s" % app.blazeio_proxy_hosts, io.anydumps(app.hosts, indent=0)[:102400])
+                        await io.plog.cyan("loaded hosts from: %s" % app.blazeio_proxy_hosts, io.anydumps(app.hosts, indent=1)[:102400])
 
             await io.sleep(app.updaters_coordination.interval)
 
