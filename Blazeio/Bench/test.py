753bdcf64949992b4f4f18b7922b17c4caf4081f
anonyxbiz <anonyxbiz@gmail.com>
2025-10-16 02:30:41 +0300

    Automated push (version: 2.7.4.1) on Wednesday 15'th October 2025 at 23:30:41 UTC

--- Blazeio/Bench/test.py
+++ Blazeio/Bench/test.py
@@ -6,18 +6,17 @@ web = io.App("0.0.0.0", 90)
 
 @web.add_route
 async def __main_handler__(r):
-    __stream_data__ = b''
     while not b'\r\n\r\n' in r.__buff__:
         await r.ensure_reading()
 
     if r.__stream__:
-        __stream_data__ = b''.join([chunk for chunk in r.__stream__])
+        await io.plog.yellow(r.__stream__)
         r.__stream__.clear()
     else:
         r.transport.resume_reading() # Allow the transport to receive more data
 
-    await r.writer(b"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: 20\r\nContent-type: text/plain\r\nDate: Wed, 15 Oct 2025 19:43:47 GMT\r\nServer: Blazeio/2.7.3.7 (posix)\r\nConnection: keep-alive\r\n\r\n{'discovered': true, '__stream_data__': '%b'}" % __stream_data__)
+    await r.writer(b"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: 20\r\nContent-type: text/plain\r\nDate: Wed, 15 Oct 2025 19:43:47 GMT\r\nServer: Blazeio/2.7.3.7 (posix)\r\nConnection: keep-alive\r\n\r\n{'discovered': true}")
 
 with web:
     web.with_keepalive()
-    web.runner()  
+    web.runner()
