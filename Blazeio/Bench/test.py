801512253495b0594146a30daac7d2527d336597
anonyxbiz <anonyxbiz@gmail.com>
2025-10-16 02:14:18 +0300

    Automated push (version: 2.7.3.8) on Wednesday 15'th October 2025 at 23:14:17 UTC

---
+++ Blazeio/Bench/test.py
@@ -0,0 +1,26 @@
+import Blazeio as io
+
+io.INBOUND_CHUNK_SIZE = 4096
+
+web = io.App("0.0.0.0", 90)
+
+
+buff = bytearray(io.INBOUND_CHUNK_SIZE)
+
+view = memoryview(buff)
+
+@web.add_route
+async def __main_handler__(r):
+    while not b'\r\n\r\n' in r.__buff__:
+        await r.ensure_reading()
+
+    if r.__stream__:
+        r.__stream__.clear()
+
+    r.__buff__memory__[:4096] = view
+
+    await r.writer(b"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: 20\r\nContent-type: text/plain\r\nDate: Wed, 15 Oct 2025 19:43:47 GMT\r\nServer: Blazeio/2.7.3.7 (posix)\r\nConnection: keep-alive\r\n\r\n{'discovered': true}")
+
+with web:
+    web.with_keepalive()
+    web.runner()
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("%s/requirements.txt" % data_path) as f:
 with open("%s/README.md" % data_path, encoding="utf-8") as f:
     long_description = f.read()
 
-version = "2.7.3.7"
+version = "2.7.3.8"
 
 exts = ("Blazeio_iourllib", "client_payload_gen", "c_request_util")
 
