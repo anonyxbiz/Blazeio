ce67561a5a738a5e6d36f2434e35d6731d9ce6d5
anonyxbiz <anonyxbiz@gmail.com>
2025-10-16 04:27:17 +0300

    Automated push (version: 2.7.5.4) on Thursday 16'th October 2025 at 01:27:17 UTC

--- Blazeio/Experimental/test0.py
+++ Blazeio/Experimental/test0.py
@@ -10,13 +10,14 @@ class Server:
         ...
 
     async def __main_handler__(app, r: io.BlazeioProtocol):
-        # while not b'\r\n\r\n' in r.__buff__:
-        if not r.__stream__:
+        while not b'\r\n\r\n' in r.__buff__:
             await r.ensure_reading()
 
         if r.__stream__:
             r.__stream__.clear() # empty the __stream__ so that we can resume reading
-
+        else:
+            await r.ensure_reading() # Wait for a chunk to be received
+    
         await r.writer(b"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: 20\r\nContent-type: text/plain\r\nDate: Wed, 15 Oct 2025 19:43:47 GMT\r\nServer: Blazeio/2.7.3.7 (posix)\r\nConnection: keep-alive\r\n\r\n{'discovered': true}")
 
 class Main(Server):
