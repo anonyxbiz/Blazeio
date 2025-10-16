1b7121103a09e5a848dd4d6be1bcfc143765516b
anonyxbiz <anonyxbiz@gmail.com>
2025-10-16 19:30:53 +0300

    Automated push (version: 2.7.6.4) on Thursday 16'th October 2025 at 16:30:52 UTC

--- Blazeio/Experimental/test0.py
+++ Blazeio/Experimental/test0.py
@@ -10,10 +10,9 @@ class Server:
         ...
 
     async def __main_handler__(app, r: io.BlazeioProtocol):
-        await r.ensure_reading()
-
-        if r.__stream__:
-            r.__stream__.clear()
+        buff = b""
+        while not "\r\n\r\n" in buff:
+            buff += await r
 
         await r.writer(b"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: 20\r\nContent-type: text/plain\r\nDate: Wed, 15 Oct 2025 19:43:47 GMT\r\nServer: Blazeio/2.7.3.7 (posix)\r\nConnection: keep-alive\r\n\r\n{'discovered': true}")
 
