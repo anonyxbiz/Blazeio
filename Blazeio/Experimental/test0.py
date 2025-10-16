4161876e29564e5c0cd242e78cbe3006efb1749a
anonyxbiz <anonyxbiz@gmail.com>
2025-10-16 19:47:56 +0300

    Automated push (version: 2.7.7.2) on Thursday 16'th October 2025 at 16:47:56 UTC

--- Blazeio/Experimental/test0.py
+++ Blazeio/Experimental/test0.py
@@ -6,17 +6,30 @@ io.INBOUND_CHUNK_SIZE = 4096
 web = io.App("0.0.0.0", 4096)
 
 class Server:
+    http_line = b"\r\n\r\n"
     def __init__(app):
         ...
     
     async def handler(app, r: io.BlazeioProtocol):
         await r.writer(b"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: 20\r\nContent-type: text/plain\r\nDate: Wed, 15 Oct 2025 19:43:47 GMT\r\nServer: Blazeio/2.7.3.7 (posix)\r\nConnection: keep-alive\r\n\r\n{'discovered': true}")
+    
+    def parser(app, r: io.BlazeioProtocol, buff: bytes):
+        idx = buff.find(app.http_line)
+        header, buff = buff[:idx], buff[idx + len(app.http_line):]
+        # Call a c-extension here to parse the request
+
+        # prepend the remainder to the request stream for the next iter to yield
+
+        r.prepend(buff)
 
     async def __main_handler__(app, r: io.BlazeioProtocol):
         buff = bytearray()
-        while not b"\r\n\r\n" in buff:
+
+        # Buffer chunks until end of headers
+        while not app.http_line in buff:
             buff.extend(await r)
-        r.prepend(buff)
+
+        app.parser(r, buff)
 
         await app.handler(r)
 
