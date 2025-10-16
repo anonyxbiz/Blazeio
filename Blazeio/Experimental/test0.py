ff93bb5e90a350b7aeb61cc8eb259256618d1817
anonyxbiz <anonyxbiz@gmail.com>
2025-10-16 19:37:40 +0300

    Automated push (version: 2.7.6.9) on Thursday 16'th October 2025 at 16:37:39 UTC

--- Blazeio/Experimental/test0.py
+++ Blazeio/Experimental/test0.py
@@ -1,7 +1,7 @@
 # Blazeio.Experimental.test0.py
 import Blazeio as io
 
-io.INBOUND_CHUNK_SIZE = 1024*1
+io.INBOUND_CHUNK_SIZE = 4096
 
 web = io.App("0.0.0.0", 4096)
 
@@ -13,6 +13,8 @@ class Server:
         buff = bytearray()
         while not b"\r\n\r\n" in buff:
             buff.extend(await r)
+        
+        r.prepend(buff)
 
         await r.writer(b"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: 20\r\nContent-type: text/plain\r\nDate: Wed, 15 Oct 2025 19:43:47 GMT\r\nServer: Blazeio/2.7.3.7 (posix)\r\nConnection: keep-alive\r\n\r\n{'discovered': true}")
 
