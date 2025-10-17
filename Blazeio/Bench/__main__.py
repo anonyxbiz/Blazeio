aca7fd0aab2e9732587ad533021aa1f37a4912a6
anonyxbiz <anonyxbiz@gmail.com>
2025-10-18 02:04:41 +0300

    Automated push (version: 2.8.1.5) on Friday 17'th October 2025 at 23:04:41 UTC

--- Blazeio/Bench/__main__.py
+++ Blazeio/Bench/__main__.py
@@ -1,5 +1,6 @@
 # Blazeio.Bench.__main__.py
 import Blazeio as io
+import Blazeio.Other.class_parser as class_parser
 
 io.ioConf.OUTBOUND_CHUNK_SIZE, io.ioConf.INBOUND_CHUNK_SIZE = 1024*100, 1024*100
 
@@ -99,7 +100,8 @@ class Client:
 class Runner(Client, Utils):
     def __init__(app):
         app.runner_notified = False
-        io.getLoop.create_task(app.runner())
+        if not app.server_only:
+            io.getLoop.create_task(app.runner())
 
     async def runner(app):
         await web # Wait for server to run
@@ -141,23 +143,19 @@ class Runner(Client, Utils):
         raise KeyboardInterrupt()
 
 class Main(Server, Runner):
-    __slots__ = ("c", "d", "payload_size", "url", "m", "payload", "serialize_connections", "conns", "writes", "runner_notified", "is_local", "analytics", "perf_counter")
+    __slots__ = ("c", "d", "payload_size", "url", "m", "payload", "serialize_connections", "conns", "writes", "runner_notified", "is_local", "analytics", "perf_counter", "server_only")
     serializer = io.ioCondition()
     sync_serializer = io.ioCondition()
     request_metrics = ("prepare", "prepare_http", "ttfb_io", "ttfb", "body_io", "latency")
     metric_types = ("elapsed", "rps")
 
-    def __init__(app, url: (str, io.Utype) = ("http://%s:%d" % (web.ServerConfig.host, web.ServerConfig.port)) + "%s", c: (int, io.Utype) = 100, d: (int, io.Utype) = 10, m: (str, io.Utype) = "get", payload_size: (int, io.Utype) = 1024, writes: (int, io.Utype) = 1, conns: (list, io.Unone) = [], serialize_connections: (bool, io.Utype) = True, analytics: (list, io.Unone) = [], perf_counter: (int, io.Unone) = io.perf_counter()):
+    def __init__(app, url: (str, io.Utype) = ("http://%s:%d" % (web.ServerConfig.host, web.ServerConfig.port)) + "%s", c: (int, io.Utype) = 100, d: (int, io.Utype) = 10, m: (str, io.Utype) = "get", payload_size: (int, io.Utype) = 1024, writes: (int, io.Utype) = 1, conns: (list, io.Unone) = [], serialize_connections: (bool, io.Utype) = True, analytics: (list, io.Unone) = [], perf_counter: (int, io.Unone) = io.perf_counter(), server_only: (bool, int, class_parser.Store, io.Utype) = False):
         io.set_from_args(app, locals(), (io.Utype, io.Unone))
         app.is_local = app.url.startswith("http://%s:%d" % (web.ServerConfig.host, web.ServerConfig.port))
         io.Super(app).__init__()
 
 if __name__ == "__main__":
-    # clear && py -m Blazeio.Versioning -update -quiet && clear && py -m Blazeio.Bench
-    from Blazeio.Other.class_parser import Parser
-    parser = Parser(Main, io.Utype)
-
     with web:
         web.with_keepalive()
-        web.attach(Main(**parser.args()))
+        web.attach(Main(**class_parser.Parser(Main, io.Utype).args()))
         web.runner()
