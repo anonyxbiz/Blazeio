b7bcd89ebbb9e507a5f0e0b4c06385c9364d9850
anonyxbiz <anonyxbiz@gmail.com>
2025-10-18 20:26:52 +0300

    Automated push (version: 2.8.2.8) on Saturday 18'th October 2025 at 17:26:51 UTC

--- Blazeio/Client/__init__.py
+++ Blazeio/Client/__init__.py
@@ -514,7 +514,7 @@ class __SessionPool__:
             instance.session.protocol = None
         
         if app.should_ensure_connected:
-            if float(perf_counter() - instance.perf_counter):
+            if method not in Session.NON_BODIED_HTTP_METHODS:
                 await app.ensure_connected(url, instance.session)
 
         return instance.session
