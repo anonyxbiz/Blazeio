3f268f72527f2d9639c0812b1bc334bcee96a7ca
anonyxbiz <anonyxbiz@gmail.com>
2025-11-13 00:32:38 +0300

    Automated push (version: 2.9.5.2) on Wednesday 12'th November 2025 at 21:32:36 UTC

--- Blazeio/__main__.py
+++ Blazeio/__main__.py
@@ -1,34 +1,41 @@
 # Blazeio.__main__.py
-from argparse import ArgumentParser
 from .Dependencies.alts import *
-from .Client import Session, getSession
+from .Client import getSession
 from os import name
+from .Other.class_parser import Positional, Parser
 
 class App:
-    def __init__(app): ...
-    
+    __slots__ = ("urls", "method", "save", "write", "headers", "slice_logs", "follow_redirects")
+    def __init__(app, urls: (str, Utype, Positional) = None, method: (str, Utype) = "get", save: (str, Utype, None) = None, write: (str, Utype, None) = None, headers: (str, Utype, None) = None, slice_logs: (int, Utype) = 0, follow_redirects: (bool, Utype) = True):
+        set_from_args(app, locals(), Utype)
+
     def default_headers(app, url, h):
         headers = { 'accept': '*/*', 'accept-language': 'en-US,en;q=0.9', 'accept-encoding': 'gzip, br', 'origin': url, 'priority': 'u=1, i', 'referer': url, 'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"%s"' % name.capitalize(), 'sec-fetch-dest': 'empty', 'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin', 'user-agent': 'BlazeI/O', 'connection': 'keep-alive', }
         if h: headers.update(h)
         return headers
+    
+    async def run(app):
+        async with Ehandler():
+            for url in app.urls.split(","):
+                await app.fetch(url)
 
-    async def fetch(app, url: (Utype, True, str) = "https://example.com", method: (Utype, None, None, str) = None, save: (Utype, None, str) = None, write: (Utype, None, str) = None, headers: (Utype, None, str) = None, slice_logs: (Utype, None, int) = 0):
+    async def fetch(app, url: str):
         if not url[:5].lower().startswith("http"): url = "https://%s" % url
 
-        headers = app.default_headers(url, {header.split("=")[0]: header.split("=")[1] for header in (headers + ",").split(",") if "=" in header} if headers else headers)
+        headers = app.default_headers(url, {header.split("=")[0]: header.split("=")[1] for header in app.headers.split(",") if "=" in header} if app.headers else app.headers)
 
         async with perf_timing() as timer:
-            async with getSession(url, method or "get", headers, follow_redirects = True) as resp:
-                if save:
-                    await resp.save(save)
-                elif write:
+            async with getSession(url, app.method or "get", headers, follow_redirects = app.follow_redirects) as resp:
+                if app.save:
+                    await resp.save(app.save)
+                elif app.write:
                     if not resp.is_prepared(): await resp.prepare_http()
 
                     buff, filename, fd = bytearray(), None, None
                     if (ext := "html") in resp.content_type.lower():
                         title_s, title_e, title = b"<title", b"</title", None
     
-                        async for chunk in resp.pull():
+                        async for chunk in resp:
                             if not fd: buff.extend(chunk)
                             if not fd and (idx := buff.find(title_s)) != -1:
                                 title = (title := buff[idx + len(title_s) + 1:])[:title.find(title_e)].decode()
@@ -38,33 +45,16 @@ class App:
                             elif fd:
                                 await fd.write(chunk)
                 else:
-                    async for chunk in resp.pull():
+                    async for chunk in resp:
                         try: chunk = chunk.decode()
                         except: ...
 
-                        await log.info(chunk if not slice_logs else chunk[:slice_logs])
+                        await log.info(chunk if not app.slice_logs else chunk[:app.slice_logs])
 
-        await plog.debug("<%s@%s%s> completed request" % (resp.args[1].lower(), resp.host, resp.path), dumps(ddict(metrics=timer.get())))
+        await plog.blue("<%s@%s%s> completed request" % (resp.args[1].lower(), resp.host, resp.path), anydumps(ddict(request = ddict(host = resp.host, path = resp.path, headers = resp.headers, status_code = resp.status_code), metrics=timer.get())))
 
 def main():
-    from argparse import ArgumentParser
-    parser = ArgumentParser(prog="Blazeio", description = "Blazeio is a cutting-edge asynchronous web server and client framework designed for building high-performance backend applications with minimal overhead.")
-    
-    # parser.add_argument('url', type=str)
-
-    for arg_count, arg in enumerate(App.fetch.__annotations__.keys()):
-        arg_type = App.fetch.__annotations__.get(arg)
-        if not isinstance(arg_type, tuple) or not Utype in arg_type: continue
-        
-        arg_data = ddict(arg = arg, required = arg_type[1], type = arg_type[2], default = App.fetch.__defaults__[arg_count])
-
-        if arg_data.required:
-            parser.add_argument(arg_data.arg, type = arg_data.type)
-        else:
-            parser.add_argument("-%s" % arg_data.arg, "--%s" % arg_data.arg, type = arg_data.type, default = arg_data.default)
-
-    args = parser.parse_args()
-    ioConf.run(App().fetch(**args.__dict__))
+    ioConf.run(App(**Parser(App, Utype).args()).run())
 
 if __name__ == "__main__":
     main()
