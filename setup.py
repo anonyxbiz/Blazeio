a570cee5fe8a5d3d60982cffcb21e6475a0ccf65
anonyxbiz <anonyxbiz@gmail.com>
2025-11-30 00:34:03 +0300

    Automated push (version: 2.9.9.2) on Saturday 29'th November 2025 at 21:34:02 UTC

--- setup.py
+++ setup.py
@@ -1,65 +1,61 @@
 from setuptools import setup, find_packages, Extension
-import os
+from datetime import datetime as dt
+from os import environ, getcwd, path, system, environ
 from platform import system as platform_system
 
-with open("requirements.txt") as f:
+data_path = path.dirname(__file__)
+
+with open("%s/requirements.txt" % data_path) as f:
     requirements = f.read().splitlines()
 
-with open("README.md", encoding="utf-8") as f:
+with open("%s/README.md" % data_path, encoding="utf-8") as f:
     long_description = f.read()
 
 version = "2.9.9.2"
 
-ext_modules = [
-    Extension(
-        "Blazeio.Blazeio_iourllib",
-        sources=["Blazeio/Extensions/Blazeio_iourllib.c"],
-        extra_compile_args=['-O3'],
-    ),
-    Extension(
-        "Blazeio.client_payload_gen", 
-        sources=["Blazeio/Extensions/client_payload_gen.c"],
-        extra_compile_args=['-O3'],
-    ),
-    Extension(
-        "Blazeio.c_request_util",
-        sources=["Blazeio/Extensions/c_request_util.c"],
-        extra_compile_args=['-O3'],
-    ),
-]
-
-if not os.environ.get("BlazeioDev"):
-    kwargs = {
-        "ext_modules": ext_modules,
-        "cmdclass": {},
-    }
-else:
-    kwargs = {}
-
-setup(
-    name="Blazeio",
-    version=version,
-    description="Blazeio",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    author="Anonyxbiz",
-    author_email="anonyxbiz@gmail.com",
-    url="https://github.com/anonyxbiz/Blazeio",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=requirements,
-    classifiers=[
+kwargs = dict(
+    name = "Blazeio",
+    version = version,
+    description = "Blazeio",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    license = "MIT",
+    author = "Anonyxbiz",
+    author_email = "anonyxbiz@gmail.com",
+    url = "https://github.com/anonyxbiz/Blazeio",
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = requirements,
+    classifiers = [
         "Programming Language :: Python :: 3",
         "Programming Language :: C",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    entry_points={
+    python_requires = '>=3.6',
+    entry_points = {
         'console_scripts': [
             'Blazeio = Blazeio.__main__:main',
         ],
     },
-    **kwargs
+    ext_modules = [Extension(
+        module,
+        sources = ["/".join(["Blazeio", "Extensions", module + ".c"])],
+        extra_compile_args=['-O3'],
+        )
+        for module in ("Blazeio_iourllib", "client_payload_gen", "c_request_util")
+    ],
+    options = dict(
+        build_ext = dict(
+            inplace = False,
+            force =  True
+        )
+    )
 )
+
+if platform_system() == 'Windows':
+    kwargs.pop("options")
+    for module in kwargs.pop("ext_modules"):
+        system("%spython %s build --mname %s --mpath %s && python %s install --mname %s --mpath %s" % ("cd %s && " % path.join(data_path, "Blazeio", "Extensions"), "setup_build.py", module["name"], path.basename(module['sources'][0]), "setup_build.py", module["name"], path.basename(module['sources'][0])))
+
+setup(**kwargs)
