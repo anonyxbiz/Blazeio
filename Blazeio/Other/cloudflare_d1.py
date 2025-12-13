d2228babb98fb4785e9f63d1d306091324376976
anonyxbiz <anonyxbiz@gmail.com>
2025-12-13 09:45:07 +0300

    Automated push (version: 3.1.5) on Saturday 13'th December 2025 at 06:45:06 UTC

---
+++ Blazeio/Other/cloudflare_d1.py
@@ -0,0 +1,36 @@
+# Blazeio.Other.cloudflare_d1
+import Blazeio as io
+
+class Client:
+    __slots__ = ("url", "headers",)
+    def __init__(app, url: str, headers: dict):
+        app.url, app.headers = url, headers
+
+    def escape(app, value):
+        if value is None:
+            return "NULL"
+        elif isinstance(value, (int, float)):
+            return str(value)
+        elif isinstance(value, bool):
+            return "1" if value else "0"
+        else:
+            escaped = str(value).replace("'", "''")
+            return "'%s'" % escaped
+
+    async def sql(app, path: str, cmd: str, *args, result_only: bool = True):
+        if args:
+            count = 0
+            while (idx := cmd.find("?")) != -1:
+                cmd = cmd[:idx] + app.escape(args[count]) + cmd[idx+1:]
+                count += 1
+
+        async with io.getSession.post(app.url + path, app.headers, json = io.ddict(sql = cmd)) as resp:
+            data = await resp.json()
+            if (result := data.get("result")) and (results := result[0].get("results")):
+                return results if len(results) > 1 else io.ddict(results[0])
+
+            if not result_only:
+                return result
+
+if __name__ == "__main__":
+    ...
