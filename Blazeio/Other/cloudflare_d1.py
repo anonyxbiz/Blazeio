55304944cf59e0b6d5f07ddf030c2b745db9cc7c
anonyxbiz <anonyxbiz@gmail.com>
2025-12-17 03:25:33 +0300

    Automated push (version: 3.1.9) on Wednesday 17'th December 2025 at 00:25:30 UTC

--- Blazeio/Other/cloudflare_d1.py
+++ Blazeio/Other/cloudflare_d1.py
@@ -10,37 +10,47 @@ class Client:
         app.endpoint = "%s/accounts/%s/d1/database/%s" % (app.base_url, app.account_id, app.database_id)
         io.create_task(app.initialize())
 
-    def __getattr__(app, key: str):
-        if key not in app.sql_paths: raise AttributeError("'%s' object has no attribute '%s'" % (app.__class__.__name__, key))
+    @property
+    def __name__(app):
+        return "Client"
 
-        def method(*args, **kwargs): return app.sql("/%s" % key, *args, **kwargs)
+    def __call__(app, *args, **kwargs):
+        return app.sql("/query", *args, **kwargs)
 
-        method.__name__ = key
-        return method
-    
+    def query(app, *args, **kwargs):
+        return app.sql("/query", *args, **kwargs)
+
     async def validate_column(app, name: str, column: str, definition: str):
-        if not await app.query("SELECT name FROM pragma_table_info('%s') WHERE name = '%s';" % (name, column)):
-            await app.query("ALTER TABLE %s ADD COLUMN %s %s;" % (name, column, definition))
+        if not await app("SELECT name FROM pragma_table_info('%s') WHERE name = '%s';" % (name, column)):
+            await app("ALTER TABLE %s ADD COLUMN %s %s;" % (name, column, definition))
+    
+    async def check_table(app, name: str, table: dict):
+        tasks = []
+        try:
+            if table.get("drop"):
+                await app("DROP TABLE IF EXISTS %s;" % name)
+
+            if isinstance(await app("SELECT name FROM sqlite_master WHERE type='table' AND name='%s';" % name), dict):
+                tasks.extend([io.create_task(app.validate_column(name, column, value)) for column, value in table.get("columns").items()])
+            else:
+                await app(cmd := "CREATE TABLE IF NOT EXISTS %s (%s%s);" % (name, (", ".join(["%s %s" % (key, value) for key, value in table.get("columns").items()])), ", " + ", ".join(list(table.get("commands"))) if table.get("commands") else ""))
+
+                if table.get("create_index"):
+                    await app("CREATE INDEX idx_%s ON %s (%s);" % (name, name, ", ".join(list(table.get("columns").keys()))))
+        finally:
+            if tasks: return await io.gather(*tasks)
 
     async def initialize(app):
         if not app.schema: return
         tasks = []
         try:
             for name, table in app.schema.get("tables").items():
-                if table.get("drop"):
-                    await app.query("DROP TABLE IF EXISTS %s;" % name)
-                if await app.query("SELECT name FROM sqlite_master WHERE type='table' AND name='%s';" % name):
-                    tasks.extend([io.create_task(app.validate_column(name, column, value)) for column, value in table.get("columns").items()])
-                else:
-                    tasks.append(io.create_task(app.query("CREATE TABLE IF NOT EXISTS %s (%s%s);" % (name, (", ".join(["%s %s" % (key, value) for key, value in table.get("columns").items()])), ", " + ", ".join(list(table.get("commands"))) if table.get("commands") else ""))))
-
-                    if table.get("create_index"):
-                        tasks.append(io.create_task(app.query("CREATE INDEX idx_%s ON %s (%s);" % (name, name, ", ".join(list(table.get("columns").keys()))))))
+                tasks.append(io.create_task(app.check_table(name, table)))
         finally:
             if tasks: await io.gather(*tasks)
 
-    async def sql(app, path: str, cmd: str, *params):
-        async with io.getSession.post(app.endpoint + path, app.headers, json = io.ddict(sql = cmd, params = list(params))) as resp:
+    async def sql(app, path: str, cmd: str, *params, batch: bool = False):
+        async with io.getSession.post(app.endpoint + path, app.headers, json = io.ddict(sql = cmd, params = list(params), batch = batch)) as resp:
             data = await resp.json()
             if (result := data.get("result")) and (results := result[0].get("results")):
                 return results if len(results) > 1 else io.ddict(results[0])
