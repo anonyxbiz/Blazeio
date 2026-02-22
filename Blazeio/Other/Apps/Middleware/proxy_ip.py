32bae69eec154e0291f655cdcefcd6c3ec57c917
anonyxbiz <anonyxbiz@gmail.com>
2026-02-22 03:01:39 +0300

    Automated push (version: 3.5.7) on Sunday 22'th February 2026 at 00:01:38 UTC

--- Blazeio/Other/Apps/Middleware/proxy_ip.py
+++ Blazeio/Other/Apps/Middleware/proxy_ip.py
@@ -10,6 +10,10 @@ class IP:
     def __call__(app, *args, **kwargs):
         return app.routes(*args, **kwargs)
 
+    async def _app_my_ip(app, r: io.BlazeioProtocol):
+        await r.prepare({"Content-type": io.Ctypes.text, "Transfer-encoding": "chunked"}, 200)
+        await r.write(r.ip_host.encode())
+
     async def before_middleware(app, r: io.BlazeioProtocol):
         if r.store is None:
             r.store = io.ddict()
