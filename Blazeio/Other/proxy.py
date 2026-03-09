6d24ef5ca5da2483703624ae4fa5828cad04ca9c
anonyxbiz <anonyxbiz@gmail.com>
2026-03-09 15:41:40 +0300

    Automated push (version: 3.6.0) on Monday 09'th March 2026 at 12:41:38 UTC

--- Blazeio/Other/proxy.py
+++ Blazeio/Other/proxy.py
@@ -307,7 +307,7 @@ class Server(Routes):
             else:
                 raise io.Abort("Bad Request", 400)
 
-        r.__miscellaneous__ = (r.__miscellaneous__[:(idx := r.__miscellaneous__.find(app.min_parser.network_config.http.one_point_one.crlf))] + b'%bip_host: "%b"%bip_port: %d' % (app.min_parser.network_config.http.one_point_one.crlf, r.ip_host.encode(), app.min_parser.network_config.http.one_point_one.crlf, r.ip_port) + r.__miscellaneous__[idx:])
+        r.__miscellaneous__ = (r.__miscellaneous__[:(idx := r.__miscellaneous__.find(app.min_parser.network_config.http.one_point_one.crlf))] + b'%bip_host: %b%bip_port: %d' % (app.min_parser.network_config.http.one_point_one.crlf, r.ip_host.encode(), app.min_parser.network_config.http.one_point_one.crlf, r.ip_port) + r.__miscellaneous__[idx:])
 
         return app.min_parser.parse(r, r.__miscellaneous__)
 
