5e466f5caba0a049bcde60bd04bace22bd6e81f2
anonyxbiz <anonyxbiz@gmail.com>
2026-05-08 23:01:55 +0300

    Automated push (version: 4.0.3) on Friday 08'th May 2026 at 20:01:51 UTC

--- Blazeio/Dependencies/Modules/delimited_startswith.py
+++ Blazeio/Dependencies/Modules/delimited_startswith.py
@@ -2,7 +2,7 @@
 from ...Dependencies import *
 
 def dstartswith(delimiter: any, data: any, sw: (tuple, list)):
-    return data[:data[len(delimiter):].find(delimiter) + len(delimiter):] in sw
+    return (data in sw or data[:data[len(delimiter):].find(delimiter) + len(delimiter):] in sw)
 
 if __name__ == "__main__":
     ...
