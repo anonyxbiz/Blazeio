2aa8531f7fd125a32230f332eb00c8dfb9b90519
anonyxbiz <anonyxbiz@gmail.com>
2026-05-21 11:12:10 +0300

    Automated push (version: 4.2.2) on Thursday 21'th May 2026 at 08:12:08 UTC

--- Blazeio/Other/sqlite3io/Modules/security.py
+++ Blazeio/Other/sqlite3io/Modules/security.py
@@ -27,4 +27,15 @@ class SignatureClient:
         current_time = int(time())
         return current_time - (current_time % window_seconds)
 
-if __name__ == "__main__": ...
+if __name__ == "__main__":
+    import Blazeio as io
+    import Blazeio.Other.class_parser as class_parser
+
+    class SignatureClientApp:
+        __slots__ = ()
+        def __init__(app, secret_key: (str, io.Utype, class_parser.Positional) = None, payload: (str, io.Utype, class_parser.Positional) = None):
+            ...
+    
+    args = class_parser.Parser(SignatureClientApp, str).args()
+
+    io.ioConf.run(io.plog.b_green(SignatureClient(args.secret_key).sign(args.payload)))
