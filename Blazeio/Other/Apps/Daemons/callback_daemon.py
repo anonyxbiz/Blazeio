b247081a0013dc4299656278b9b81cf6294bb513
anonyxbiz <anonyxbiz@gmail.com>
2026-06-27 11:04:24 +0300

    Automated push (version: 4.4.1) on Saturday 27'th June 2026 at 08:04:22 UTC

--- Blazeio/Other/Apps/Daemons/callback_daemon.py
+++ Blazeio/Other/Apps/Daemons/callback_daemon.py
@@ -2,14 +2,15 @@
 import Blazeio as io
 
 class CbDaemon:
-    __slots__ = ("callbacks", "event", "task")
-    def __init__(app, callbacks, event: (io.SharpEvent, None) = None):
-        app.callbacks, app.event, app.task = list(callbacks), event or io.SharpEvent(), io.create_task(app.daemon())
+    __slots__ = ("callbacks", "event", "asleep", "task")
+    def __init__(app, callbacks, event: (io.SharpEvent, None) = None, asleep: int = 60):
+        app.callbacks, app.event, app.asleep, app.task = list(callbacks), event or io.SharpEvent(), asleep, io.create_task(app.daemon())
 
     async def daemon(app):
         while True:
             await app.event.wait_clear()
-            for callback in app.callbacks:
-                callback()
+            for callback in app.callbacks: callback()
+
+            await io.sleep(app.asleep) # Sleep in between to avoid rapid wakeups
 
 if __name__ == "__main__": ...
