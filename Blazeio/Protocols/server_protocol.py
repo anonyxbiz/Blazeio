cbd19cba53f1556861f7270e220b0abaab480ad8
anonyxbiz <anonyxbiz@gmail.com>
2026-09-09 03:00:14 +0300

    Automated push (version: 4.5.9) on Wednesday 09'th September 2026 at 00:00:13 UTC

--- Blazeio/Protocols/server_protocol.py
+++ Blazeio/Protocols/server_protocol.py
@@ -58,7 +58,7 @@ class BlazeioServerProtocol(BlazeioProtocol, BufferedProtocol, BlazeioPayloadUti
     def state(app, truncante: bool = True):
         s = {key: str(value)[:500] if truncante and not isinstance(value, (int, str)) else value for key in app.__class__.__slots__ if (value := getattr(app, key, "")) or 1}
         
-        if isinstance(s.store, dict):
+        if isinstance(s.get("store"), dict):
             s.update(s.store)
 
         return s
