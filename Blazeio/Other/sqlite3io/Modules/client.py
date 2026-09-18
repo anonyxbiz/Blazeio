63e3cbf6f8546e784bf44cfa6ca2f3f6648ea6b5
anonyxbiz <anonyxbiz@gmail.com>
2026-09-18 19:12:15 +0300

    Automated push (version: 4.6.5) on Friday 18'th September 2026 at 16:12:14 UTC

--- Blazeio/Other/sqlite3io/Modules/client.py
+++ Blazeio/Other/sqlite3io/Modules/client.py
@@ -213,7 +213,7 @@ class SqlSession(Modules, Migrators):
         data = io.ddict(q = q, parameters = [])
         for i in parameters:
             if not isinstance(i, (tuple, list)):
-                raise io.Err("Blazeio.Other.sqlite3io.Modules.client::SqlSession.executemany", "parameter value must be a tuple or list")
+                raise io.Err("Blazeio.Other.sqlite3io.Modules.client::SqlSession.executemany", "parameter value must be a tuple or list, got: %s" % str(i))
 
             data.parameters.append(tuple([app.sanitize_value(value) for value in i]))
 
