1f869e937b28966bac96be18944afb7bfa3a0474
anonyxbiz <anonyxbiz@gmail.com>
2026-09-20 23:08:21 +0300

    Automated push (version: 4.7.0) on Sunday 20'th September 2026 at 20:08:19 UTC

--- Blazeio/Other/sqlite3io/Modules/server.py
+++ Blazeio/Other/sqlite3io/Modules/server.py
@@ -168,14 +168,15 @@ class Server:
 
         await r.prepare({"Transfer-encoding": "chunked", "Content-type": "video/mp4", "Cache-Control": "no-store, no-cache, must-revalidate, private", "Cloudflare-CDN-Cache-Control": "no-store, no-cache", "Pragma": "no-cache", "X-Accel-Buffering": "no"}, 200)
         
-        with query.conn:
+        async with app.cond:
             try:
                 query.cursor.execute(*tuple(query.args))
             except Exception as e:
                 raise io.Eof(await r.write(app.mux(query.delimiter, io.dumps(io.ddict(error = str(e))))))
-    
-        if not query.cursor.description:
-            raise io.Eof(await r.write(app.mux(query.delimiter, io.dumps(io.ddict(success = True)))), io.Scope.Sql.Events.add_event(query.form, query.cursor))
+        
+            if not query.cursor.description:
+                query.conn.commit()
+                raise io.Eof(await r.write(app.mux(query.delimiter, io.dumps(io.ddict(success = True)))), io.Scope.Sql.Events.add_event(query.form, query.cursor))
 
         columns = [col[0] for col in query.cursor.description]
 
@@ -189,14 +190,15 @@ class Server:
 
         await r.prepare({"Transfer-encoding": "chunked", "Content-type": "video/mp4", "Cache-Control": "no-store, no-cache, must-revalidate, private", "Cloudflare-CDN-Cache-Control": "no-store, no-cache", "Pragma": "no-cache", "X-Accel-Buffering": "no"}, 200)
         
-        with query.conn:
+        async with app.cond:
             try:
                 query.cursor.executemany(*tuple(query.args))
             except Exception as e:
                 raise io.Eof(await r.write(app.mux(query.delimiter, io.dumps(io.ddict(error = str(e))))))
-
-        if not query.cursor.description:
-            raise io.Eof(await r.write(app.mux(query.delimiter, io.dumps(io.ddict(success = True)))), io.Scope.Sql.Events.add_event(query.form, query.cursor))
+    
+            if not query.cursor.description:
+                query.conn.commit()
+                raise io.Eof(await r.write(app.mux(query.delimiter, io.dumps(io.ddict(success = True)))), io.Scope.Sql.Events.add_event(query.form, query.cursor))
 
         columns = [col[0] for col in query.cursor.description]
 
